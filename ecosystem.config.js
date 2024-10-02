864b3b4e561cdf607d816a0a532a53a1b96ce969
Daniel Garcia <contacto@danigarcia.org>
2024-10-02 18:32:08 +0200

    fix: update pm2 configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    apps: [
-        {
-            name: "pw-educamos-notifier",
-            script: "npm",
-            automation: false,
-            args: "run start-pm2"
-        }
-    ]
-}
+  apps: [
+    {
+      name: "pw-educamos-notifier",
+      script: "npm",
+      args: "run once",
+      watch: false,
+      autorestart: false,
+      cron_restart: "*/30 * * * *",
+    },
+  ],
+};
